6c02341fa330fb09a07954749f8c2e0c0077d98c
takenori-sasa <yoshida.tkw@gmail.com>
2023-04-26 12:15:09 +0900

    👍#1 多対多relation作成ちょっと不満

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,10 @@
 class Card < ApplicationRecord
+  has_many :children_relationships, class_name: 'Edge',
+                                    foreign_key: 'child_id',
+                                    dependent: :destroy
+  has_many :children, through: :children_relationships, source: :child
+  has_many :parents_relationships, class_name: 'Edge',
+                                   foreign_key: 'parent_id',
+                                   dependent: :destroy
+  has_many :parents, through: :parents_relationships, source: :parent
 end
