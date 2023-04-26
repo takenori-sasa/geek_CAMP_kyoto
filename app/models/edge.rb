840ed01a4cca12764717554629d083da652a403f
takenori-sasa <yoshida.tkw@gmail.com>
2023-04-26 13:09:31 +0900

    👍#1 self_loop排除

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -3,4 +3,11 @@ class Edge < ApplicationRecord
   belongs_to :child, class_name: 'Card'
   validates :parent_id, presence: true
   validates :child_id, presence: true
+  validate :self_loop?
+
+  def self_loop?
+    return unless parent_id.eql?(child_id)
+
+    errors.add(:parent_id, 'must be other than Child')
+  end
 end
