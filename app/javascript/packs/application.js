657c3705f85dcd5b63c06d7f74e0d6a7b292841a
takenori-sasa <yoshida.tkw@gmail.com>
2023-04-29 11:09:08 +0900

    👍#4 bootstrap追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,10 @@ require("@rails/ujs").start();
 require("@rails/activestorage").start();
 require("channels");
 require("./googlemap");
-
+//以下の2行
+import 'bootstrap';
+import '../stylesheets/application.scss';
+import '@fortawesome/fontawesome-free/js/all';
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
